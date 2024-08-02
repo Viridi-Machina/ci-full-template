d273de3d7b726d42dcb0b71a9737033b1266d5d0
jacen green <jacen.l.green@hotmail.com>
2024-08-02 08:09:11 +0100

    added image filter option to model with serializer validation

--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,7 @@ from django.http import Http404
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+
 from .models import Profile
 from .serializers import ProfileSerializer
 from drf_api.permissions import IsOwnerOrReadOnly
